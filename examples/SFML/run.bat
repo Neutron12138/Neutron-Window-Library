492dd8aa43472310fe154b4038016af4d53a11bc
Neutron12138 <121910770+Neutron12138@users.noreply.github.com>
2023-04-29 21:15:42 +0800

    Delete examples directory

--- examples/SFML/run.bat
+++
@@ -1,6 +0,0 @@
-set filename=main
-g++ -finput-charset=UTF-8 -fexec-charset=gbk ^
-    "%filename%.cpp" -o "%filename%.exe" ^
-    -lglfw3 -lgdi32 -lopengl32 -lglew32 -lfreeglut -lsfml-graphics -lsfml-window -lsfml-system ^
-    -I ../../includes/
-"%filename%.exe"
